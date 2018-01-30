57259c043bcd475104047775b715b6dd9f5cdef4
Daxuan Shu <daxuan@g.ucla.edu>
2018-01-30 09:31:19 -0800

    rm ID

--- Lab/Datalab/bits.c
+++ Lab/Datalab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Daxuan Shu : 204853061>
+ * <Daxuan Shu : 2*******1>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
